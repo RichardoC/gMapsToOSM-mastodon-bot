a8380732d1c58c19b0f991d0b5c5c33f6353f9f7
Richard Tweed <RichardoC@users.noreply.github.com>
2025-11-18 23:36:21 +0000

    Terrible in progress code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # gMapsToOSM-mastodon-bot
 Code for https://mastodon.bot/@gMapsToOSM
+
+
+
+
+## Running locally
+
+### Set up a local mastodon instance
+<https://github.com/mastodon/mastodon/blob/main/docs/DEVELOPMENT.md#docker>
+
+curl 'http://localhost:3000/api/v1/statuses' \
+  -X POST \
+  --data-raw '{"status":"boop","spoiler_text":"","in_reply_to_id":"115573172928989452","media_ids":[],"sensitive":false,"visibility":"public","poll":null,"language":"en","quoted_status_id":null,"quote_approval_policy":"public"}'
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net/url"
-	"os"
+	// "fmt"
+	zlog "log"
 
 	"github.com/mattn/go-mastodon"
 	"github.com/thought-machine/go-flags"
@@ -14,96 +12,50 @@ import (
 )
 
 type Options struct {
-	Server   string `long:"server" description:"Mastodon server to connect to" default:"https://mastodon.bot"`
-	Verbose          bool   `long:"verbosity" short:"v" description:"Uses zap Development default verbose mode rather than production"`
-}
-
-func ConfigureClient(server string) {
-	appConfig := &mastodon.AppConfig{
-		Server:       server,
-		ClientName:   "publicApp",
-		Scopes:       "read write follow",
-		Website:      "https://github.com/mattn/go-mastodon",
-		RedirectURIs: "urn:ietf:wg:oauth:2.0:oob",
-	}
-
-	app, err := mastodon.RegisterApp(context.Background(), appConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Have the user manually get the token and send it back to us
-	u, err := url.Parse(app.AuthURI)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Open your browser to \n%s\n and copy/paste the given authroization code\n", u)
-	var userAuthorizationCode string
-	fmt.Print("Paste the code here:")
-	fmt.Scanln(&userAuthorizationCode)
-
-	config := &mastodon.Config{
-		Server:       "https://mastodon.social",
-		ClientID:     app.ClientID,
-		ClientSecret: app.ClientSecret,
-	}
-
-	// Create the client
-	c := mastodon.NewClient(config)
-
-	// Exchange the User authentication code with an access token, that can be used to interact with the api on behalf of the user
-	err = c.GetUserAccessToken(context.Background(), userAuthorizationCode, app.RedirectURI)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Lets Export the secrets so we can use them later to preform actions on behalf of the user
-	// Without having to request authroization all the time.
-	// Exporting this as Environment variables, but it can be a configuration file, or database, anywhere you'd like to keep this credentials
-	os.Setenv("MASTODON_CLIENT_ID", c.Config.ClientID)
-	os.Setenv("MASTODON_CLIENT_SECRET", c.Config.ClientSecret)
-	os.Setenv("MASTODON_ACCESS_TOKEN", c.Config.AccessToken)
+	Server       string `long:"server" description:"Mastodon server to connect to" default:"https://mastodon.bot"`
+	ClientID     string `long:"client-id" description:"Mastodon application client ID"`
+	ClientSecret string `long:"client-secret" description:"Mastodon application client secret"`
+	AccessToken  string `long:"access-token" description:"Mastodon application access token"`
+	Verbose      bool   `long:"verbosity" short:"v" description:"Uses zap Development default verbose mode rather than production"`
 }
 
 // Preform user actions wihtout having to re-authenticate again
-func doUserActions() {
-	// Load Environment variables, config file, secrets from db
-	clientID := os.Getenv("MASTODON_CLIENT_ID")
-	clientSecret := os.Getenv("MASTODON_CLIENT_SECRET")
-	accessToken := os.Getenv("MASTODON_ACCESS_TOKEN")
-
-	config := &mastodon.Config{
-		Server:       "https://mastodon.social",
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		AccessToken:  accessToken,
-	}
+func doUserActions(log *zap.SugaredLogger, config *mastodon.Config) {
 
 	// instanciate the new client
 	c := mastodon.NewClient(config)
 
 	// Let's do some actions on behalf of the user!
-	acct, err := c.GetAccountCurrentUser(context.Background())
+	var pg mastodon.Pagination
+	notifs, err := c.GetNotifications(context.Background(), &pg)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to get notifications", "error", err)
 	}
-	fmt.Printf("Account is %v\n", acct)
-
-	finalText := "this is the content of my new post!"
-	visibility := "public"
-
-	// Post a toot
-	toot := mastodon.Toot{
-		Status:     finalText,
-		Visibility: visibility,
+	for _, n := range notifs {
+		log.Info("Got notification", "notification", n, "type", n.Type)
+		log.Info("Is the type a mention?", "mention", n.Type == "mention")
 	}
-	post, err := c.PostStatus(context.Background(), &toot)
+	// filter only for mentions
+	// do the replies, with a pool of goroutines to ensure we don't do too many?
+	// dismiss the notification once we do the reply
+	// continue paging through
+	// fmt.Printf("notifications are %+v\n", notifs)
 
-	if err != nil {
-		log.Fatalf("%#v\n", err)
-	}
+	// finalText := "this is the content of my new post!"
+	// visibility := "public"
 
-	fmt.Printf("My new post is %v\n", post)
+	// // Post a toot
+	// toot := mastodon.Toot{
+	// 	Status:     finalText,
+	// 	Visibility: visibility,
+	// }
+	// post, err := c.PostStatus(context.Background(), &toot)
+
+	// if err != nil {
+	// 	log.Fatalf("%#v\n", err)
+	// }
+
+	// fmt.Printf("My new post is %v\n", post)
 
 }
 
@@ -113,7 +65,7 @@ func main() {
 	parser := flags.NewParser(&opts, flags.Default)
 	_, err := parser.Parse()
 	if err != nil {
-		log.Fatalf("can't parse flags: %v", err)
+		zlog.Fatalf("can't parse flags: %v", err)
 	}
 
 	var z *zap.Logger
@@ -125,15 +77,21 @@ func main() {
 		z = zap.Must(zap.NewProduction())
 	}
 	defer z.Sync()
-	logger := z.Sugar()
-
-	logger.Infow("Got config", "config", opts)
+	log := z.Sugar()
 
 	// Set maxprocs and have it use our nice logger
-	maxprocs.Set(maxprocs.Logger(logger.Infof))
+	maxprocs.Set(maxprocs.Logger(log.Infof))
 	undo := zap.RedirectStdLog(z)
 	defer undo()
 
-	ConfigureClient(opts.Server)
-	doUserActions()
+	config := &mastodon.Config{
+		Server:       opts.Server,
+		ClientID:     opts.ClientID,
+		ClientSecret: opts.ClientSecret,
+		AccessToken:  opts.AccessToken,
+	}
+	// check credentials work
+	// do the actual thing, try not to do too many things at once, and have backoffs if we get 429s
+	// polling loop
+	doUserActions(log, config)
 }
